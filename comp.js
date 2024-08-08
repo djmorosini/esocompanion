4b88369442c402eb1a5d3ef505acc4a95965c9d6
Dylan Morosini <djmorosini@outlook.com>
2024-08-08 11:01:05 -0400

    updates for dropdown menu, test

--- comp.js
+++ comp.js
@@ -23,12 +23,20 @@ function openIframe(evt, pageName) {
   evt.currentTarget.className += " active";
 }
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+const dropdownBtn = document.getElementById("icon");
+const dropdownMenu = document.getElementById("dropdown");
+
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+};
+
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
   }
-}
+});
